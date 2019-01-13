b9686b4533a262057f0cefc7531a72248219c69f
rmnbrtshk <rmnbrtshk@gmail.com>
2019-01-13 14:09:43 +0200

    fix showCurrentArrOfRows function

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # events_js
-[first link](https://bartushak007.github.io/events_js/src/)
-
-[second table link](https://bartushak007.github.io/events_js/src/last_task/)
--- src/last_task/main.js
+++ src/last_task/main.js
@@ -1,184 +1,242 @@
-var filterValue = document.querySelector('.filter__input');
-var rowsValue = document.querySelector('.show__input');
-var tableElements = document.querySelectorAll('.country');
-var rowsValue = document.querySelector('.show__input');
-var show = document.querySelector('.show-number');
+(function() {
+  var filterValue = document.querySelector('.filter__input');
+  var pagerPage = document.querySelectorAll('.pager__page');
+  var tableElements = document.querySelectorAll('.country');
+  var rowsValue = document.querySelector('.show__input');
+  var show = document.querySelector('.show-number');
   var to = document.querySelector('.to-number');
   var of = document.querySelector('.of-number');
-var page = 0;
-var arrOfVisibleElements = [];
-x();
-doArrOfVisibleElements();
+  var page = 0;
+  var arrOfVisibleElements = [];
 
-function arrIndexListStart() {
-  var arr = [];
-
-  for (var i = 0; i < tableElements.length; i++) {
-    if (!tableElements[i].parentNode.hidden) {
-      arr.push(i);
-    }
-  }
-
-  return arr;
-}
-
-filterValue.addEventListener('keyup', filter);
-filterValue.addEventListener('keyup', doArrOfVisibleElements);
-filterValue.addEventListener('keyup', function() {
-  showCurrentArrOfRows(arrOfVisibleElements, 0);
-});
-
-function filter() {
-  var filterValue = document.querySelector('.filter__input');
-  visibleTableElementsArrIndex = [];
-
-  if (filterValue.tagName === 'INPUT') {
-    tableElements.forEach = [].forEach;
-    tableElements.forEach(function(item, i) {
-      var regExp = new RegExp('^(' + filterValue.value + ')', 'i');
-
-      if (item.innerHTML.search(regExp) == -1) {
-        item.parentNode.hidden = true;
-      } else {
-        item.parentNode.hidden = false;
-        visibleTableElementsArrIndex.push(i);
-      }
-    });
-  }
-}
-
-rowsValue.addEventListener('click', doArrOfVisibleElements);
-rowsValue.addEventListener('click', function() {
-  showCurrentArrOfRows(arrOfVisibleElements, 0);
-});
-rowsValue.addEventListener('keyup', doArrOfVisibleElements);
-rowsValue.addEventListener('keyup', function() {
-  showCurrentArrOfRows(arrOfVisibleElements, 0);
-});
-
-function doArrOfVisibleElements() {  
-  var counter = 0;
-  arrOfVisibleElements = [];
-  visibleTableElementsArrIndex = arrIndexListStart();
-
-  for (var i = 0; i < visibleTableElementsArrIndex.length; i++) {
-    if (i % +rowsValue.value === 0) {
-      arrOfVisibleElements.push([]);
-      counter++;
-    }
-    arrOfVisibleElements[counter - 1].push(visibleTableElementsArrIndex[i]);
-  }
-  console.log('counter' + counter);
-}
-
-function showCurrentArrOfRows(arr, x) {
-  
-  show.innerHTML = 1;
-  to.innerHTML = rowsValue.value;
-  
-
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr[i].length; j++) {
-      if (i !== x) {
-        tableElements[arr[i][j]].parentNode.style.display = 'none';
-      } else {
-        tableElements[arr[i][j]].parentNode.style.display = '';
-      }
-    }
-  }
-  var pagerPage = document.querySelectorAll('.pager__page');
-  var lastPage = (pagerPage[pagerPage.length - 1].innerHTML =
-    arrOfVisibleElements.length);
-
-  if (x === 0) {
-    var seconPage = (pagerPage[1].innerHTML = 2);
-    var thirdCurrentPage = (pagerPage[2].innerHTML = 3);
-    var fourthPage = (pagerPage[3].innerHTML = 4);
-  }
-
-  if (x === arr.length - 1) {
-    seconPage = pagerPage[1].innerHTML = arr.length - 3;
-    thirdCurrentPage = pagerPage[2].innerHTML = arr.length - 2;
-    fourthPage = pagerPage[3].innerHTML = arr.length - 1;
-  }
-
-  if (x > 1 && x < arr.length - 2) {
-    seconPage = pagerPage[1].innerHTML = x;
-    thirdCurrentPage = pagerPage[2].innerHTML = x + 1;
-    fourthPage = pagerPage[3].innerHTML = x + 2;
-  }
-
-  if (arr.length <= 4) {
-    seconPage = pagerPage[1].innerHTML = 1;
-    thirdCurrentPage = pagerPage[2].innerHTML = 2;
-    fourthPage = pagerPage[3].innerHTML = 3;
-  }
-
-  if (arr.length <= 3) {
-    seconPage = pagerPage[1].innerHTML = 2;
-    thirdCurrentPage = pagerPage[2].innerHTML = 2;
-    fourthPage = pagerPage[3].innerHTML = 2;
-  }
-
-  if (arr.length <= 2) {
-    seconPage = pagerPage[1].innerHTML = 1;
-    thirdCurrentPage = pagerPage[2].innerHTML = 1;
-    fourthPage = pagerPage[3].innerHTML = 1;
-  }
-
-}
-
-var controls = document.querySelector('.pager__controls');
-controls.addEventListener('click', function() {
-  event.preventDefault();
-});
-
-controls.addEventListener('click', function() {
-  var prev = document.querySelector('.pager__prev');
-  var next = document.querySelector('.pager__next');  
-  var pagerPage = document.querySelectorAll('.pager__page');
-
-  if (event.target == prev && page > 0) {
-    showCurrentArrOfRows(arrOfVisibleElements, --page);
-  } else if (event.target == next && page < arrOfVisibleElements.length) {
-    showCurrentArrOfRows(arrOfVisibleElements, ++page);
-  } else if (
-    event.target.tagName === 'A' &&
-    !(event.target === prev && !(event.target === next))
-  ) {
-    page = +event.target.innerHTML - 1;
-
-    showCurrentArrOfRows(arrOfVisibleElements, page);
-  }
-
-  // console.log(page);
-  // controls.addEventListener('click', x);
-});
-
-controls.addEventListener('click', x);
-showCurrentArrOfRows(arrOfVisibleElements, 0);
-
-function x() {
-  var pagerPage = document.querySelectorAll('.pager__page');
-  var count = 0;
-  for (var i = 0; i < pagerPage.length; i++) {
-    for (var j = 0; j < pagerPage.length; j++) {
-      // if (!(document.querySelector('.pager__page--current') != null)) {
-      //   // pagerPage[page].classList.remove('pager__page--current');
-      // } else console.log(document.querySelector('.pager__page--current'));
-      count++;
-      // if (pagerPage[count].classList.contains('pager__page--current')) {
-      //   console.log('good')
-      //   pagerPage[count].classList.remove('pager__page--current');
-      // }
-    }
-
-    if (+pagerPage[i].innerHTML === page + 1) {
-      pagerPage[i].classList.add('pager__page--current');      
-    }
-  }
-  
-  show.innerHTML = (page*10+1);
-  to.innerHTML = (page*10) + +rowsValue.value;
   of.innerHTML = tableElements.length;
-}
+
+  filterValue.addEventListener('keyup', filter);
+  filterValue.addEventListener('keyup', doArrOfVisibleElements);
+  filterValue.addEventListener('keyup', function() {
+    showCurrentArrOfRows(arrOfVisibleElements, 0);
+  });
+
+  rowsValue.addEventListener('input', doArrOfVisibleElements);
+  rowsValue.addEventListener('input', function() {
+    showCurrentArrOfRows(arrOfVisibleElements, 0);
+    var pagerPageAll = document.querySelectorAll('.pager__page--current');
+
+    for (var i = 0; i < pagerPage.length; i++) {
+      if (pagerPageAll[i]) {
+        pagerPageAll[i].classList.remove('pager__page--current');
+      }
+    }
+    pagerPage[0].classList.add('pager__page--current');
+  });
+
+  var controls = document.querySelector('.pager__controls');
+  controls.addEventListener('click', function() {
+    event.preventDefault();
+  });
+
+  controls.addEventListener('click', function() {
+    var prev = document.querySelector('.pager__prev');
+    var next = document.querySelector('.pager__next');
+    var evTarget = event.target;
+
+    if (evTarget === prev && page > 0) {
+      showCurrentArrOfRows(arrOfVisibleElements, --page);
+    } else if (evTarget === next) {
+      if (page + 1 < arrOfVisibleElements.length) {
+        showCurrentArrOfRows(arrOfVisibleElements, ++page);
+      }
+    } else if (
+      evTarget.tagName === 'A' && !(evTarget === prev && !(evTarget === next))) {
+      page = +evTarget.innerHTML - 1;
+      showCurrentArrOfRows(arrOfVisibleElements, page);
+    }
+    if (page + 1 === arrOfVisibleElements.length) {
+      next.classList.remove('pager__next--available');
+    } else {
+      next.classList.add('pager__next--available');
+    }
+
+    if (page === 0) {
+      prev.classList.remove('pager__prev--available');
+    } else {
+      prev.classList.add('pager__prev--available');
+    }
+  });
+
+  doArrOfVisibleElements();
+  showCurrentArrOfRows(arrOfVisibleElements, 0);
+
+  function arrIndexListStart() {
+    var arr = [];
+
+    for (var i = 0; i < tableElements.length; i++) {
+      if (!tableElements[i].parentNode.hidden) {
+        arr.push(i);
+      }
+    }
+
+    return arr;
+  }
+
+  function filter() {
+    var filterValue = document.querySelector('.filter__input');
+    var visibleTableElementsArrIndex = [];
+
+    if (filterValue.tagName === 'INPUT') {
+      tableElements.forEach = [].forEach;
+      tableElements.forEach(function(item, i) {
+        var regExp = new RegExp('^(' + filterValue.value + ')', 'i');
+
+        if (item.innerHTML.search(regExp) == -1) {
+          item.parentNode.hidden = true;
+        } else {
+          item.parentNode.hidden = false;
+          visibleTableElementsArrIndex.push(i);
+        }
+      });
+    }
+  }
+
+  function doArrOfVisibleElements() {
+    var counter = 0;
+    var pagerPageAll = document.querySelectorAll('.pager__page--current');
+    arrOfVisibleElements = [];
+    visibleTableElementsArrIndex = arrIndexListStart();
+
+    for (var i = 0; i < visibleTableElementsArrIndex.length; i++) {
+      if (i % +rowsValue.value === 0) {
+        arrOfVisibleElements.push([]);
+        counter++;
+      }
+      arrOfVisibleElements[counter - 1].push(visibleTableElementsArrIndex[i]);
+    }    
+  }
+
+  function showCurrentArrOfRows(arr, x) {
+    var controls = document.querySelector('.pager__controls');
+    var firstDots = document.querySelector('.pager__list-item-first');
+    var secondDots = document.querySelector('.pager__list-item--second');
+
+    var calcElementsVisible = 0;
+    newArr = [];
+
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = 0; j < arr[i].length; j++) {
+        calcElementsVisible++;
+        if (x < arr.length) {
+          if (i !== x) {
+            tableElements[arr[i][j]].parentNode.style.display = 'none';
+          } else {
+            tableElements[arr[i][j]].parentNode.style.display = '';
+            newArr.push(calcElementsVisible);
+          }
+        }
+      }
+    }
+
+    show.innerHTML = newArr[0];
+    to.innerHTML = newArr[newArr.length - 1];
+
+    var lastPage = pagerPage[pagerPage.length - 1].innerHTML = arrOfVisibleElements.length;
+
+    if (x === 0) {
+      var seconPage = (pagerPage[1].innerHTML = 2);
+      var thirdCurrentPage = (pagerPage[2].innerHTML = 3);
+      var fourthPage = (pagerPage[3].innerHTML = 4);
+    }
+
+    if (x > 2) {
+      firstDots.style.display = '';
+    } else {
+      firstDots.style.display = 'none';
+    }
+
+    if (arrOfVisibleElements.length < 5) {
+      pagerPage[1].style.display = 'none';
+    } else {
+      pagerPage[1].style.display = '';
+    }
+
+    if (arrOfVisibleElements.length < 4) {
+      pagerPage[3].style.display = 'none';
+    } else {
+      pagerPage[3].style.display = '';
+    }
+
+    if (arrOfVisibleElements.length < 3) {
+      pagerPage[2].style.display = 'none';
+    } else {
+      pagerPage[2].style.display = '';
+    }
+
+    if (arrOfVisibleElements.length < 2) {
+      controls.style.display = 'none';
+    } else {
+      controls.style.display = '';
+    }
+
+    if (x < arrOfVisibleElements.length - 3) {
+      secondDots.style.display = '';
+    } else {
+      secondDots.style.display = 'none';
+    }
+
+    if (arrOfVisibleElements.length < 6) {
+      firstDots.style.display = 'none';
+      secondDots.style.display = 'none';
+    }
+
+    if (x === arr.length - 1) {
+      seconPage = pagerPage[1].innerHTML = arr.length - 3;
+      thirdCurrentPage = pagerPage[2].innerHTML = arr.length - 2;
+      fourthPage = pagerPage[3].innerHTML = arr.length - 1;
+    }
+
+    if (x > 1 && x < arr.length - 2) {
+      seconPage = pagerPage[1].innerHTML = x;
+      thirdCurrentPage = pagerPage[2].innerHTML = x + 1;
+      fourthPage = pagerPage[3].innerHTML = x + 2;
+    }
+
+    if (arr.length <= 4) {
+      seconPage = pagerPage[1].innerHTML = 1;
+      thirdCurrentPage = pagerPage[2].innerHTML = 2;
+      fourthPage = pagerPage[3].innerHTML = 3;
+    }
+
+    if (arr.length <= 3) {
+      seconPage = pagerPage[1].innerHTML = 2;
+      thirdCurrentPage = pagerPage[2].innerHTML = 2;
+      fourthPage = pagerPage[3].innerHTML = 2;
+    }
+
+    if (arr.length <= 2) {
+      seconPage = pagerPage[1].innerHTML = 1;
+      thirdCurrentPage = pagerPage[2].innerHTML = 1;
+      fourthPage = pagerPage[3].innerHTML = 1;
+    }
+
+    changeCurrentPageNumber();
+  }
+
+  function changeCurrentPageNumber() {
+    var pagerPageAll = document.querySelectorAll('.pager__page--current');
+
+    for (var i = 0; i < pagerPageAll.length; i++) {
+      if (pagerPageAll[i]) {
+        pagerPageAll[i].classList.remove('pager__page--current');
+      }
+    }
+
+    for (var i = 0; i < pagerPage.length; i++) {
+      if (pagerPageAll[i]) {
+        pagerPageAll[i].classList.remove('pager__page--current');
+      }
+
+      if (+pagerPage[i].innerHTML === page + 1) {
+        pagerPage[i].classList.add('pager__page--current');
+      }
+    }
+  }
+})();
